1541557c193e95837ae418b9e59374da3b400074
Matias Sotelo <matias-sotelo@Matias.machine>
2020-09-05 17:25:15 -0300

    se agrego footer, falta completar

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>Formulario Login</title>
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
     </head>
 
     <body>
@@ -31,9 +32,28 @@
 
         </form>
         
+        
+        <footer class="pie">
+            <section class="mostrador">
+            <i class="fab fa-github"></i>
+                <p><a href="https://github.com/sotelomati">Sotelomati</a></p>
+                <p><a href="https://github.com/GonzalezAg">GonzalezAg</a></p>
+                <p><a href="https://github.com/schmidtoctavio">SchmidtOctavio</a></p>
+            </section>
 
+            <section class="mostrador">
+                <i class="fab fa-github"></i>
+                
+            </section>
 
+            <section class="mostrador">
+                <i><img src="images/uader.png" alt="imagen de uader"></i>
+            </section>
+
+            <hr>
+            <section class="pieFinal">
+            <i class="far fa-registered">Copyright Todos los derechos reservados</i>
+            </section>
+        </footer>
     </body>
-
-
 </html>
