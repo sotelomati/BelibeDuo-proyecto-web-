63eab10c0489428461062e597bd92e91a9be449f
Your Name <you@example.com>
2020-10-04 22:49:28 -0300

    se restablecio navegacion

--- include/navigation.php
+++ include/navigation.php
@@ -6,8 +6,25 @@
     <title>Document</title>
 </head>
 <body>   
-    <ul class="menuLeft" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+<ul class="menuLeft" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
 
+<a href="index.php">
+<li>Home</li>
+</a>
+<a href="#"></a>
+
+<a href="#">
+<li>More</li>
+</a>
+</ul>
+
+<div class="contenedorNav">
+<nav class="navegacion">
+    
+    <li>
+        <i class="fas fa-blog" style="font-size: 40px; float: left"></i>
+    </li>
+    <ul class="menuLeft_borrar">
         <a href="index.php">
         <li>Home</li>
         </a>
@@ -16,27 +33,42 @@
         <a href="#">
         <li>More</li>
         </a>
-    </ul>
 
-    <div class="contenedorNav">
-        <nav class="navegacion">
+        <!--<hr class="lineaNav">-->
+    </ul>   
+    
+    
+
+        <ul class= dch>
             
-            <li>
-                <i class="fas fa-blog" style="font-size: 40px; float: left"></i>
-            </li>
-            <ul class="menuLeft_borrar">
-                <a href="index.php">
-                <li>Home</li>
-                </a>
-                <a href="#"></a>
+             <?php 
+                $url= $_SERVER["REQUEST_URI"];
+                $remplazar =""; //variable que se mostrara en la esquina superior derecha
+                $ruta= ""; 
+                
+                if(($url == "/loginPA/index.php") OR ($url == "/loginPA/")){
+                    $remplazar= "Login";
+                    $ruta="login.php";
+                }
+                elseif($url == "/loginPA/logueado.php"){
+                    $remplazar = "Log Out";
+                    $ruta="index.php";
+                }
+                else{
+                    $remplazar = "";
+                }
+                
+                
+            ?>
 
-                <a href="#">
-                <li>More</li>
-                </a>
-
-                <!--<hr class="lineaNav">-->
-            </ul>   
-        </nav>   
-    </div>
+            <a href=<?php echo $ruta; ?>>
+                <li> <?php echo $remplazar; ?></li>
+            </a>
+                <div class="prueba">
+                <i class="fas fa-bars"></i>
+                </div>
+        </ul>
+</nav>   
+</div>
 </body>
 </html>
