76c6210f81cbc43f581e8d3156dbd8ba62c0de88
Octavio <schmidt.octavio@gmail.com>
2020-10-22 02:29:57 -0300

    Se modificaron estilos

--- inicio.php
+++ inicio.php
@@ -9,7 +9,17 @@
         <div class="correcto">
             <h1>Ingreso Correctamente</h1>
             <i  class="fas fa-check"></i>    
-    </div>   
+        </div>  
+        <div class="juegos">
+            <?php
+                //LLAMADA A LA BASE DE DATOS
+                
+                foreach($juegos as $juego){
+                    echo '<div class="juegos"> <h1>'.$juego[0]'</h1></div>'
+                }
+            ?>
+        
+        </div>
     <?php require_once 'include/footer.php';?> 
 </body>
 
