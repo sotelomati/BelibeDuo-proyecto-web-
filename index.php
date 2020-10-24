e7fe349c51afe3ae34ba445058a219a4ef57a4cf
Your Name <you@example.com>
2020-10-23 23:17:58 -0300

    se agrego galeria de juegos, falta conectar con base de datos

--- index.php
+++ index.php
@@ -5,7 +5,77 @@
     <title>Principal</title>
 </head>
 <body class="body">
-    
+    <div class="galeria">
+        <div class="contenedorJuegos">
+                    <?php
+                        //LLAMADA A LA BASE DE DATOS
+                        /*
+                        foreach($juegos as $juego){
+                            echo '<div class="juegos"> <h1>'.$juego[0]'</h1></div>'
+                        }*/
+                    
+                            echo '<div class="juego"> 
+                                <h1> League of Legends </h1>
+                                <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laudantium deserunt praesentium nisi temporibus earum minus beatae necessitatibus consequatur? Velit porro accusantium magnam tempore nihil et culpa, quam ullam nostrum.
+                                </p>
+                                <input class="buttons" type="submit" id="juego1" name="detalles" value="Detalles">
+                            </div>';
+                            echo '<div class="juego"> 
+                                <h1> League of Legends </h1>
+                                <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laudantium deserunt praesentium nisi temporibus earum minus beatae necessitatibus consequatur? Velit porro accusantium magnam tempore nihil et culpa, quam ullam nostrum.
+                                </p>
+                                <input class="buttons" type="submit" id="juego1" name="detalles" value="Detalles">
+                            </div>';
+                            echo '<div class="juego"> 
+                                <h1> League of Legends </h1>
+                                <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laudantium deserunt praesentium nisi temporibus earum minus beatae necessitatibus consequatur? Velit porro accusantium magnam tempore nihil et culpa, quam ullam nostrum.
+                                </p>
+                                <input class="buttons" type="submit" id="juego1" name="detalles" value="Detalles">
+                            </div>';
+
+                            echo '<div class="juego"> 
+                                <h1> League of Legends </h1>
+                                <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laudantium deserunt praesentium nisi temporibus earum minus beatae necessitatibus consequatur? Velit porro accusantium magnam tempore nihil et culpa, quam ullam nostrum.
+                                </p>
+                                <input class="buttons" type="submit" id="juego1" name="detalles" value="Detalles">
+                            </div>';
+                            echo '<div class="juego"> 
+                                <h1> League of Legends </h1>
+                                <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laudantium deserunt praesentium nisi temporibus earum minus beatae necessitatibus consequatur? Velit porro accusantium magnam tempore nihil et culpa, quam ullam nostrum.
+                                </p>
+                                <input class="buttons" type="submit" id="juego1" name="detalles" value="Detalles">
+                            </div>';
+                            echo '<div class="juego"> 
+                                <h1> League of Legends </h1>
+                                <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laudantium deserunt praesentium nisi temporibus earum minus beatae necessitatibus consequatur? Velit porro accusantium magnam tempore nihil et culpa, quam ullam nostrum.
+                                </p>
+                                <input class="buttons" type="submit" id="juego1" name="detalles" value="Detalles">
+                            </div>';
+                            echo '<div class="juego"> 
+                                <h1> League of Legends </h1>
+                                <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laudantium deserunt praesentium nisi temporibus earum minus beatae necessitatibus consequatur? Velit porro accusantium magnam tempore nihil et culpa, quam ullam nostrum.
+                                </p>
+                                <input class="buttons" type="submit" id="juego1" name="detalles" value="Detalles">
+                            </div>';
+                            echo '<div class="juego"> 
+                                <h1> League of Legends </h1>
+                                <p>
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur laudantium deserunt praesentium nisi temporibus earum minus beatae necessitatibus consequatur? Velit porro accusantium magnam tempore nihil et culpa, quam ullam nostrum.
+                                </p>
+                                <input class="buttons" type="submit" id="juego1" name="detalles" value="Detalles">
+                            </div>';
+                            
+                        
+                    ?>
+            </div>
+        </div>
     <?php require_once 'include/footer.php';?> 
 </body>
 
--- inicio.php
+++ inicio.php
@@ -10,14 +10,7 @@
             <h1>Ingreso Correctamente</h1>
             <i  class="fas fa-check"></i>    
         </div>  
-        <div class="juegos">
-            <?php
-                //LLAMADA A LA BASE DE DATOS
-                
-                foreach($juegos as $juego){
-                    echo '<div class="juegos"> <h1>'.$juego[0]'</h1></div>'
-                }
-            ?>
+        
         
         </div>
     <?php require_once 'include/footer.php';?> 
